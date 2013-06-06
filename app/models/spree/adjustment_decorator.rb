e8dffee6b7304a4708f0f616fc02df9cd5046045
Brandon Carpenter <bhcarpenter@gmail.com>
2013-06-06 09:28:37 -0400

    Force the adjustments to recalculate in order
    1. Shipping
    2. Promotions
    3. Tax
    4. Credits

--- app/models/spree/adjustment_decorator.rb
+++ app/models/spree/adjustment_decorator.rb
@@ -1,3 +1,4 @@
 Spree::Adjustment.class_eval do
+  default_scope :order => "FIELD(#{self.table_name}.originator_type, \"Spree::ShippingMethod\", \"Spree::PromotionAction\", \"Spree::TaxRate\", \"Spree::OrderCredit\") ASC"
   scope :store_credits, lambda { where(:originator_type => 'Spree::OrderCredit') }
 end
