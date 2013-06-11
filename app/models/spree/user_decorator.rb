a242925ae516af3f77b5e9dc15a67f3e0c4b210c
Brandon Carpenter <bhcarpenter@gmail.com>
2013-06-11 14:03:00 -0400

    Grant pending credits to newly-registered users.

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,6 +2,8 @@ if Spree.user_class
   Spree.user_class.class_eval do
     has_many :store_credits, :class_name => "Spree::StoreCredit"
 
+    after_create :apply_pending_credits
+
     def has_store_credit?
       store_credits.present?
     end
@@ -9,5 +11,15 @@ if Spree.user_class
     def store_credits_total
       store_credits.sum(:remaining_amount)
     end
+
+    protected
+    def apply_pending_credits
+      Spree::StoreCredit.where(:email => email, :issued_on => nil).each do |credit|
+        credit.user = self
+        credit.issued_on = DateTime.now
+        credit.save
+      end
+    end
+
   end
 end
